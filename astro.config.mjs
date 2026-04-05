317f4e05ab2a5b0430d82a3fceebcd065173b04b
xkeshav <xkeshav@gmail.com>
2026-04-05 22:04:38 +0530

    partytown added; social preview image updated; rename astro files as per feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [
+    react(), 
+    mdx(), 
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ],
   output: "static",
   site: "https://www.threadzip.com",
   base: "/",
   trailingSlash: "ignore",
-  prefetch: true,
-  //adapter: node({
-  //  mode: "standalone",
-  //}),
+  prefetch: true
 });
