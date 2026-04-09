70cdb9b1d80b2c5be8cadace75ea3002061a8de2
xkeshav <xkeshav@gmail.com>
2026-04-09 23:59:21 +0530

    added faqs

---
+++ src/utils/messages.ts
@@ -0,0 +1,62 @@
+import type { Question } from "../models";
+
+export const FAQ_LIST: Question[] = [
+  {
+    question: "What is threadzip?",
+    answer:
+      "Threadzip is our comprehensive showcase platform designed to help textile raw material industry wholesaler. This platform is a dedicated B2B solution for the raw textile industry, particularly focused on shopkeepers and wholesalers in Surat. It helps them showcase their raw materials, manage inventory, and sell their stock more efficiently.",
+  },
+  {
+    question: "What are your products?",
+    answer:
+      "Our flagship product is the ThreadZip platform, offering you to enlist your unsold and current inventory with one click.",
+  },
+  {
+    question: "What are your services?",
+    answer: "Our services primarily revolve around providing an showcase platform, ensuring seamless experience.",
+  },
+  {
+    question: "How it is different from other marketplace?",
+    answer:
+      "Unlike general marketplaces, this platform is built specifically for the raw textile industry. It focuses on solving unique challenges such as leftover stock management and connecting B2B sellers to relevant buyers.",
+  },
+  {
+    question: "How does this platform help sellers?",
+    answer:
+      "The platform offers several benefits to sellers, including: A space to showcase their raw materials. Tools to manage leftover stock and sell it quickly. Opportunities to connect with new buyers. A streamlined process for inventory management and transactions.",
+  },
+  {
+    question: "What type of materials does this platform support?",
+    answer:
+      "The platform supports a variety of raw textile materials, like 1. Fabrics used in the production of garments. 2. Textile designs and patterns.",
+  },
+  {
+    question: "Who is this platform designed for?",
+    answer:
+      "The platform is specifically made for: 1. Shopkeepers and wholesalers in the raw textile industry. 2. Businesses involved in B2B transactions, particularly in Surat.",
+  },
+  {
+    question: "What problem does this platform solve?",
+    answer: `Shopkeepers in Surat, who deal with raw textile materials, often face these challenges: 1. Managing leftover stock (unsold inventory). 2. Finding new buyers beyond their local reach. 3.
+Showcasing their products effectively to potential customers. This platform is designed to solve these problems by connecting sellers with buyers through a user-friendly application.`,
+  },
+  {
+    question: " Why focus on Surat?",
+    answer:
+      " Surat is a hub for the textile industry, with over 64,000 shopkeepers and wholesalers dealing in raw materials. This city plays a significant role in the industry, but many sellers face challenges in selling their stock or expanding their business. That’s why the platform is initially focusing on Surat.",
+  },
+  {
+    question: "Why focus on the raw textile industry?",
+    answer:
+      "The raw textile industry, particularly in Surat, forms a significant part of the economy. Shopkeepers and wholesalers often face challenges in managing unsold stock and reaching new markets. This platform is designed to address their pain points.",
+  },
+  {
+    question: "Is this platform limited to Surat?",
+    answer:
+      "Initially, the platform is being launched exclusively for Surat, considering the large number of wholesalers and shopkeepers in the city. It may expand to other cities in the future.",
+  },
+  {
+    question: "Is this free?",
+    answer: "yes, it is completely free to use and upload your inventory photos but with a different approach",
+  },
+];
